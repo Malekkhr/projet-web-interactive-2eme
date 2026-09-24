ea44c2638516cbecf50cbdeda4e309beca8c51ee
Malekkhr <malek.khadhraoui@esprit.tn>
2026-09-24 10:31:28 +0100

    Add files via upload

---
+++ project web interactive/javascript/script.js
@@ -0,0 +1,22 @@
+var X = 25;
+var age = 30;
+console.log(age);
+console.log("je".toUpperCase());
+console.log("je".toLowerCase());
+console.log("Bonjour".indexOf("jour"));
+console.log("danstamererer".includes("tamerer"));
+let S = "S";
+console.log("S" == S); // true
+
+
+var note = 9;
+if(note >= 16){
+    console.log("très bien");
+} else if(note >= 14){
+    console.log(" bien");
+    } else if(note >= 12){
+    console.log(" Assez bien");
+    } else if(note >= 10){
+    console.log(" Passable");
+    } else 
+    console.log(" nul");
